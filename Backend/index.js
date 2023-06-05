7c43a4a74820b861ce2e194f14e02b0b3a4028c1
Sandeep Bhai <mail4sandeep44@gmail.com>
2023-06-05 09:59:18 +0530

    starting

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req,res) =>
+{
+    res.send("Yes Crud Application")
+})
+
+
+app.listen(3500, () =>
+{
+    console.log("Running at Port 3500")
+})
